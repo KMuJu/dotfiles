4a437f4ddc380a743d5ee5e6f70d8ac1da6b0e05
Kasper <kasper.mustad.juell@linux.com>
2024-09-08 23:00:17 +0200

    Add hotkeys to control windows and workspaces with multiple monitors

--- config/.config/i3/xss_lock.sh
+++ config/.config/i3/xss_lock.sh
@@ -1,2 +1,2 @@
 xset s 210 180
-xss-lock -n ~/.local/scripts/dim-screen2.sh -- i3lock -n --image /tmp/bgblur.png
+xss-lock -n ~/.local/scripts/dim-screen2.sh -- (i3lock -n --image /tmp/bgblur.png > $HOME/locklog)
