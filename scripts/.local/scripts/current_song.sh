f6afb77d77d36b1a8d462ac25b5bd5ad6036733a
Kasper <kasper.mustad.juell@linux.com>
2024-12-08 14:25:28 +0100

    feat: Send notifications with progressbar each second until script is run again

--- scripts/.local/scripts/current_song.sh
+++ scripts/.local/scripts/current_song.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+status=$(playerctl --player=spotify status 2 2> /dev/null)
+if [[ "$status" != "Playing" ]]; then
+    echo "Not playing"
+    exit 0
+fi
+
+playingPath="/tmp/dunstPlaying"
 idPath="/tmp/dunstID"
 songPath="/tmp/song"
 prevUrlPath="/tmp/songUrl"
 prevURL=$(cat "$prevUrlPath")
 id=$(cat $idPath)
+playing=$(cat $playingPath)
 
 echo $id
-count=$(dunstctl count displayed)
-echo "Count $count"
-if [[ $count != 0 ]]; then
+if [[ $playing == 1 ]]; then
     dunstify --close=$id
     echo -1 > $idPath
+    echo 0 > $playingPath
     echo "Closed"
     exit
 fi
+echo 1 > $playingPath
 
 name=$(playerctl --player=spotify metadata -f "{{playerName}}")
 title=$(playerctl --player=spotify metadata -f "{{title}}")
@@ -34,11 +42,24 @@ if [[ $prevURL != $artUrl ]]; then
     echo $artUrl > $prevUrlPath
 fi
 
-id=$(dunstify -p -i "$songPath" \
-    "$title" "$artist" \
-    -u low \
-    -h int:value:$percent\ # show how percentage of the 
-)
+p=$percent
 
-echo $id > $idPath
-echo $id
+while [ $(cat $playingPath) == 1 ]; do
+    position=$(playerctl --player=spotify metadata -f '{{position}}')
+    if [ -n "$position" ] && [ -n "$length" ]; then
+        # Perform the calculation to get the percentage
+        percent=$(echo "scale=4; $position / $length * 100" | bc)
+    fi
+    if [[ $p != $percent ]]; then
+        id=$(dunstify -p -i "$songPath" \
+            "$title" "$artist" \
+            -u low \
+            -h int:value:$percent\
+            -t 0 \
+        )
+
+        echo $id > $idPath
+        p=$percent
+    fi
+    sleep 1
+done
