5724af3bd9a155f8496377c305524e793c1d3d37
Kasper <kasper.mustad.juell@linux.com>
2024-12-12 21:15:57 +0100

    add kitty and commit everything

---
+++ scripts/.local/scripts/open_floating_kitty.sh
@@ -0,0 +1,4 @@
+
+# kitty --title "floating_kitty" bash -c "~/.local/scripts/select_workspace.sh" 2> /dev/null &
+
+kitty --title "floating_kitty" -c "~/.local/scripts/select_workspace.sh" 2> /dev/null
---
+++ scripts/.local/scripts/select_workspace.sh
@@ -0,0 +1,22 @@
+# workspaces=$(i3-msg -t get_tree | jq '[recurse(.nodes[]?, .floating_nodes[]?) | select(.type == "workspace") | {workspace: .name, windows: [.nodes[]?, .floating_nodes[]? | recurse(.nodes[]?, .floating_nodes[]?) | select(.window_properties != null) | .window_properties.class]}]')
+workspaces=$(i3-msg -t get_tree | jq -r '
+    [recurse(.nodes[]?, .floating_nodes[]?) | select(.type == "workspace" and .name != "__i3_scratch") | 
+    {workspace: .name, windows: [.nodes[]?, .floating_nodes[]? | recurse(.nodes[]?, .floating_nodes[]?) | 
+    select(.window_properties != null and .name != "__i3_scratch") | .window_properties.class]}] |
+    map("\(.workspace): \(.windows | join(", "))") | join("\n")
+')
+# echo $workspaces
+
+# workspaces=$(i3-msg -t get_workspaces | jq -r '.[].name')
+# echo $workspaces
+
+# Show the list in fzf for selection
+selected=$(echo "$workspaces" | fzf --prompt="Select a workspace: " --height=20% --border --reverse)
+
+# Extract the workspace name (text before the colon) if something is selected
+if [ -n "$selected" ]; then
+  workspace=$(echo "$selected" | cut -d':' -f1)
+  i3-msg workspace "$workspace"
+else
+  echo "No workspace selected"
+fi
