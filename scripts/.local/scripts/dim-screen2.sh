f37f6b9a84a68d062bd5351c5e828bd1c66e1e4d
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-04-24 12:42:30 +0200

    script to dim screen
    A bit blocky

--- scripts/.local/scripts/dim-screen2.sh
+++ scripts/.local/scripts/dim-screen2.sh
@@ -6,16 +6,32 @@
 # (this prevents the screen from flashing before it is turned completely off
 # by DPMS in the locker command).
 
-min_brightness=30
-fade_time=2000
-fade_steps=70
+min_brightness=0
+fade_time=5000
+fade_steps=200
 
-BRIGHTNESS=$(xbacklight -get)
-trap "xbacklight -set $BRIGHTNESS" EXIT
-trap 'kill %%; exit 0' HUP
-trap 'sleep 1s; kill %%; exit 0' TERM
+current_brightness=$(brightnessctl g)
+max_brightness=$(brightnessctl m)
+# echo $current_brightness
+# trap "brightnessctl -set $current_brightness" EXIT
+# trap 'kill %%; exit 0' HUP
+# trap 'sleep 1s; kill %%; exit 0' TERM
 
-xbacklight -time $fade_time -steps $fade_steps -set $min_brightness &
-wait
-sleep infinity & # No, sleeping in the foreground does not work
-wait
+
+current_percent=$(( current_brightness * 100 / max_brightness ))
+step_size=$(( (current_percent - min_brightness) / fade_steps ))
+step_delay=$(( fade_time / fade_steps ))  # Milliseconds per step
+
+# Ensure step size is at least 1
+if [ "$step_size" -lt 1 ]; then
+    step_size=1
+fi
+
+# Fade brightness down
+for ((i = current_percent; i > min_brightness; i -= step_size)); do
+    brightnessctl set "${i}%"
+    sleep $(echo "$step_delay / 1000" | bc -l)  # Convert ms to seconds
+done
+
+# Set final brightness
+brightnessctl set "${min_brightness}"
