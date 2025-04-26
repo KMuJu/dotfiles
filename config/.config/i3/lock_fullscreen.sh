3c7f3d8234999028684c7f5331830a50e63eca35
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-04-26 15:20:29 +0200

    log locking to /tmp

--- config/.config/i3/lock_fullscreen.sh
+++ config/.config/i3/lock_fullscreen.sh
@@ -3,11 +3,11 @@
 active_window=$(xprop -root | grep '_NET_ACTIVE_WINDOW(WINDOW)' | awk '{print $5}')
 current_brightness=$(cat /tmp/brightness)
 if [ "$active_window" != "0x0" ] && xprop -id "$active_window" | grep -q "_NET_WM_STATE_FULLSCREEN"; then
-    echo "Window is in fullscreen" > ~/lock_log
+    echo "Window is in fullscreen" > /tmp/lock_log
     exit 0  # Don't lock
 else
     i3lock -c 1f1f1f
-    echo "locking $current_brightness" > ~/lock_log
+    echo "locking $current_brightness" > /tmp/lock_log
 fi
 
 brightnessctl set $current_brightness
