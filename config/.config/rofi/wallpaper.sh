1da224794718e4cbf7a386f934ab4538341665aa
Kasper Mustad Juell <kasper.mustad.juell@gmail.com>
2025-10-13 22:27:10 -0300

    feat(rofi): Add background selector

---
+++ config/.config/rofi/wallpaper.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#  ██╗    ██╗ █████╗ ██╗     ██╗     ██████╗  █████╗ ██████╗ ███████╗██████╗
+#  ██║    ██║██╔══██╗██║     ██║     ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+#  ██║ █╗ ██║███████║██║     ██║     ██████╔╝███████║██████╔╝█████╗  ██████╔╝
+#  ██║███╗██║██╔══██║██║     ██║     ██╔═══╝ ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗
+#  ╚███╔███╔╝██║  ██║███████╗███████╗██║     ██║  ██║██║     ███████╗██║  ██║
+#   ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+#
+#  ██╗      █████╗ ██╗   ██╗███╗   ██╗ ██████╗██╗  ██╗███████╗██████╗
+#  ██║     ██╔══██╗██║   ██║████╗  ██║██╔════╝██║  ██║██╔════╝██╔══██╗
+#  ██║     ███████║██║   ██║██╔██╗ ██║██║     ███████║█████╗  ██████╔╝
+#  ██║     ██╔══██║██║   ██║██║╚██╗██║██║     ██╔══██║██╔══╝  ██╔══██╗
+#  ███████╗██║  ██║╚██████╔╝██║ ╚████║╚██████╗██║  ██║███████╗██║  ██║
+#  ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+#	originally written by: gh0stzk - https://github.com/gh0stzk/dotfiles
+#	rewritten for hyprland by :	 develcooking - https://github.com/KMuJu/dotfiles
+#	Info    - This script runs the rofi launcher, to select
+#             the wallpapers included in the theme you are in.
+
+
+
+# Set some variables
+wall_dir="${HOME}/Bilder/Backgrounds/"
+cacheDir="${HOME}/.cache/backgrounds/${theme}"
+rofi_command="rofi -x11 -dmenu -theme ${HOME}/dotfiles/config/.config/rofi/wallSelect.rasi -theme-str ${rofi_override}"
+
+# Create cache dir if not exists
+if [ ! -d "${cacheDir}" ] ; then
+    mkdir -p "${cacheDir}"
+fi
+
+
+physical_monitor_size=14
+monitor_res=$(hyprctl monitors |grep -A2 Monitor |head -n 2 |awk '{print $1}' | grep -oE '^[0-9]+')
+dotsperinch=$(echo "scale=2; $monitor_res / $physical_monitor_size" | bc | xargs printf "%.0f")
+monitor_res=$(( $monitor_res * $physical_monitor_size / $dotsperinch ))
+
+rofi_override="element-icon{size:${monitor_res}px;border-radius:0px;}"
+
+# Convert images in directory and save to cache dir
+for image in "$wall_dir"/*.{jpg,jpeg,png,webp}; do
+	if [ -f "$image" ]; then
+		name=$(basename "$image")
+        if [ ! -f "${cacheDir}/${name}" ] ; then
+            magic convert -strip "$image" -thumbnail 500x500^ -gravity center -extent 500x500 "${cacheDir}/${name}"
+        fi
+    fi
+done
+
+# Select a picture with rofi
+wall_selection=$(find "${wall_dir}"  -maxdepth 1  -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec basename {} \; | sort | while read -r A ; do  echo -en "$A\x00icon\x1f""${cacheDir}"/"$A\n" ; done | $rofi_command)
+
+# Set the wallpaper
+[[ -n "$wall_selection" ]] || exit 1
+${HOME}/.config/hypr/scripts/change_wallpaper.sh ${wall_dir}/${wall_selection}
+
+exit 0
